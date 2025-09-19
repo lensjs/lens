8b35a6b61467c72224831ba55ae2670bc9b7e2be
Mohamed Attar <mohammedattar0100020@gmail.com>
2025-09-19 10:29:58 +0300

    feat(apps-fastify): cache demo for testing

--- apps/fastify/src/index.ts
+++ apps/fastify/src/index.ts
@@ -1,60 +1,81 @@
 import Fastify from "fastify";
 import { lens } from "@lensjs/fastify";
-import fastifyStatic from "@fastify/static";
 import * as path from "node:path";
 import { fileURLToPath } from "node:url";
-import { lensContext } from "@lensjs/core";
+import MemoryCache from "./concrete/cache/memory_cache.js";
 
-const fastify = Fastify({ logger: true });
+const fastify = Fastify({
+  logger: true,
+});
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const assetsPath = path.join(__dirname, "..", "assets");
+const cache = new MemoryCache();
 
-// pass false so lens doesn't double-register
 const { logException } = await lens({
   app: fastify,
-  registerFastifyStatic: true,
+  cacheWatcherEnabled: true,
 });
 
-// EITHER: handle static yourself
-await fastify.register(fastifyStatic, {
-  root: path.join(__dirname, "..", "assets"),
-  prefix: "/assets/",
-  wildcard: false,
-  decorateReply: false,
-});
-
-await fastify.register(fastifyStatic, {
-  root: path.join(__dirname, "..", "assets"),
-  prefix: "/file/",
-  serve: false,
-  wildcard: false,
-  decorateReply: false,
-});
-
-fastify.get("/", async (_, reply) => {
-  return { hello: "world" };
+// Routes
+fastify.get("/", async () => {
+  return Promise.resolve({ hello: "world" });
 });
 
 fastify.get("/file", (_, reply) => {
   reply.sendFile("example.pdf", assetsPath);
 });
 
-fastify.get("/error", async (_, reply) => {
-  console.log("store", lensContext.getStore());
+fastify.get("/error", async () => {
   throw new Error("Something went wrong");
 });
 
-fastify.setErrorHandler((err, req, reply) => {
+// Cache Routes
+
+fastify.get("/get-cache", () => {
+  return {
+    value: cache.get("key"),
+    message: "Successfully fetched cache",
+  };
+});
+
+fastify.get("/set-cache", () => {
+  cache.set("key", "value");
+
+  return {
+    message: "Cache set successfully",
+  };
+});
+
+fastify.get("/delete-cache", () => {
+  cache.delete("key");
+
+  return {
+    message: "Cache deleted successfully",
+  };
+});
+
+fastify.get("/has-cache", () => {
+  return {
+    value: cache.has("key"),
+    message: "Cache has successfully",
+  };
+});
+
+fastify.get("/clear-cache", () => {
+  cache.clear();
+
+  return {
+    message: "Cache cleared successfully",
+  };
+});
+
+// Error Handler
+fastify.setErrorHandler((err, req) => {
   logException(err, req);
 
   throw err;
 });
 
-fastify.setErrorHandler((err, req, reply) => {
-  logException(err, req);
-
-  throw err;
-});
 await fastify.listen({ port: 3000 });
 
 console.log("Server listening on http://localhost:3000");
